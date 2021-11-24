ea5faa096311d490cce4cc59357b75f646df8654
Nguyen Phat <phatnt.uit@gmail.com>
2021-11-25 00:23:33 +0700

    prepare for typescript version

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,13 @@
 const path = require('path');
-const jsConfig = require('./jsconfig.json');
+const tsConfig = require('./tsconfig.json');
 
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
   plugins: [
     [
       'module-resolver',
       {
-        root: [path.resolve(jsConfig.compilerOptions.baseUrl)],
+        root: [path.resolve(__dirname, tsConfig.compilerOptions.baseUrl)],
       },
     ],
   ],
--- src/util/common/number.ts
+++ src/util/common/number.ts
@@ -1,12 +1,12 @@
 import { sum, subtract } from 'lib/math';
 
-export const commonPlus = (num1, num2) => {
+export const commonPlus = (num1: number, num2: number) => {
   const result = sum(num1, num2);
   console.log(`commonPlus: ${num1} + ${num2} = ${result}`);
   return result;
 };
 
-export const commonSubtract = (num1, num2) => {
+export const commonSubtract = (num1: number, num2: number) => {
   const result = subtract(num1, num2);
   console.log(`commonSubtract: ${num1} - ${num2} = ${result}`);
   return result;
